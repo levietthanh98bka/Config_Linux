a9142199504abb162d7b718f29930f9cde82fd1a
thanh <levietthanh98bka@gmail.com>
2026-08-23 23:03:43 +0700

    O

--- setup.sh
+++ setup.sh
@@ -1,62 +1,5 @@
 #!/usr/bin/env bash
 
-# Source path
-SCRIPT_PATH=$(realpath "$(dirname $0)")
-ALL_CONFIGS=${SCRIPT_PATH}/configs
-
-# Config home
-USER_CONFIGS=$HOME/.config
-
-CONFIGS=("fish" "kitty" "nvim" "bash")
-
-append_bashrc() {
-	LINE='source "$HOME/.config/bash/init.sh"'
-	BASHRC="$HOME/.bashrc"
-
-	if ! grep -Fxq "$LINE" "$BASHRC"; then
-		echo "$LINE" >>"$BASHRC"
-	fi
-	source $BASHRC
-}
-
-setup_dotfiles() {
-	echo "Setup dotfiles."
-	echo "--------------------------------------"
-
-	if test $(pwd) != $SCRIPT_PATH; then
-		echo "cd $SCRIPT_PATH"
-		cd ${SCRIPT_PATH}
-		echo "--------------------------s------------"
-	fi
-
-	echo "Backup or unlink old config"
-	for config in "${CONFIGS[@]}"; do
-		local conf_path=${USER_CONFIGS}/${config}
-		local conf_path_bak=${USER_CONFIGS}/${config}.bak
-
-		if [[ -L $conf_path ]]; then
-			echo "unlink $conf_path"
-			unlink $conf_path
-		elif [[ -d $conf_path ]]; then
-			rm -rf $conf_path_bak
-			echo "Rename: $conf_path -> $conf_path_bak"
-			mv $conf_path $conf_path_bak
-		fi
-	done
-	echo "--------------------------------------"
-
-	echo "Symlink new configs"
-	for config in "${CONFIGS[@]}"; do
-		echo "Create symlink: ${USER_CONFIGS}/${config} -> ${ALL_CONFIGS}/${config}"
-		ln -s ${ALL_CONFIGS}/${config} ${USER_CONFIGS}/${config}
-	done
-	echo "--------------------------------------"
-	append_bashrc
-	echo "set -g KFC_GIT_STATUS true" | tee ~/.config/fish/conf.d/user_config.fish
-	echo
-	echo "Completed."
-}
-
 installFish() {
 	echo "Install fish: start..."
 	sudo add-apt-repository ppa:fish-shell/release-3 -y
@@ -106,21 +49,91 @@ installFonts() {
 	echo "Install fonts: end!!!"
 }
 
+# Source path
+SCRIPT_PATH=$(realpath "$(dirname $0)")
+ALL_CONFIGS=${SCRIPT_PATH}/configs
+
+# Config home
+USER_CONFIGS=$HOME/.config
+CONFIGS=("fish" "kitty" "nvim" "bash")
+
+append_bashrc() {
+	LINE='source "$HOME/.config/bash/init.sh"'
+	BASHRC="$HOME/.bashrc"
+
+	if ! grep -Fxq "$LINE" "$BASHRC"; then
+		echo "$LINE" >>"$BASHRC"
+	fi
+	source $BASHRC
+}
+
+setup_dotfiles() {
+	echo "Setup dotfiles."
+	echo "--------------------------------------"
+
+	if test $(pwd) != $SCRIPT_PATH; then
+		echo "cd $SCRIPT_PATH"
+		cd ${SCRIPT_PATH}
+		echo "--------------------------s------------"
+	fi
+
+	echo "Backup or unlink old config"
+	for config in "${CONFIGS[@]}"; do
+		local conf_path=${USER_CONFIGS}/${config}
+		local conf_path_bak=${USER_CONFIGS}/${config}.bak
+
+		if [[ -L $conf_path ]]; then
+			echo "unlink $conf_path"
+			unlink $conf_path
+		elif [[ -d $conf_path ]]; then
+			rm -rf $conf_path_bak
+			echo "Rename: $conf_path -> $conf_path_bak"
+			mv $conf_path $conf_path_bak
+		fi
+	done
+	echo "--------------------------------------"
+
+	echo "Symlink new configs"
+	for config in "${CONFIGS[@]}"; do
+		echo "Create symlink: ${USER_CONFIGS}/${config} -> ${ALL_CONFIGS}/${config}"
+		ln -s ${ALL_CONFIGS}/${config} ${USER_CONFIGS}/${config}
+	done
+	echo "--------------------------------------"
+	append_bashrc
+	echo "set -g TFC_GIT_STATUS true" | tee ~/.config/fish/conf.d/user_config.fish
+	echo
+	echo "Completed."
+}
+
+
 main() {
 
 	while true; do
 		echo "====================Setup Ubuntu===================="
-		echo "1: Install common(git,g++,gcc) "
+		echo "666: Install all"
+		echo "1: Install common(git,g++,gcc)"
 		echo "2: Install Fish"
 		echo "3: Install Kitty"
 		echo "4: Install Neovim"
-		echo "5: Install Fronts"
+		echo "5: Install Fonts"
 		echo "6: Config dotfile"
 		echo "0: Exit"
 
 		read -p "Choose: " choose
 
 		case "$choose" in
+		666)
+			echo "Install all: start..."
+			sudo apt update
+			sudo apt install -y gcc g++
+			sudo apt install -y git
+			installFish
+			installKitty
+			installNeovim
+			installFonts
+			setup_dotfiles
+			echo "Install all: end!!!"
+			;;
 		1)
 			echo "Install common: start..."
 			sudo apt update
